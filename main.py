fceb8d54462954f25c266e09fecf3a6cac852b04
Bntuhan <batuhan120806@gmail.com>
2025-05-18 11:09:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ else:
     
 haberler=c.fetchall()
 
+if len(haberler)==0:
+    st.warning(f" {ara} sorgusu ile ilgili Herhangi Bir Haber bulunamadı")
+
 for i in range(0,len(haberler),3):
     col1,col2,col3=st.columns(3)
 
