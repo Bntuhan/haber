0ab264d0d5a9765b689b33c08defe34934f3a784
Bntuhan <batuhan120806@gmail.com>
2025-05-18 15:37:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ ara=st.text_input("Haber İçinde Arama Yap")
 
 conn=sqlitecloud.connect('sqlitecloud://cyql96oxhk.g3.sqlite.cloud:8860/chinook.sqlite?apikey=VCgODu4MWtgTc4FfUmMWQwdhrYpj0WRs9vhFtNIgEB4')
 c=conn.cursor()
-
+placeholders = ','.join('?' for _ in dilsecimi)
+query = f"SELECT * FROM haberler WHERE dil IN ({placeholders}) ORDER BY trend_id DESC LIMIT 99"
+c.execute(query, dilsecimi)
 
 if len(ara) > 1:
     if len(dilsecimi) > 0:
