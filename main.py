ff4373363717ab2071cb331df1a1483cbd301c4c
Bntuhan <batuhan120806@gmail.com>
2025-05-18 15:42:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,31 +19,39 @@ if guncelle:
 dilsecimi=st.multiselect("Ülke Seç",diller)
 ara=st.text_input("Haber İçinde Arama Yap")
 
-conn=sqlitecloud.connect('sqlitecloud://cyql96oxhk.g3.sqlite.cloud:8860/chinook.sqlite?apikey=VCgODu4MWtgTc4FfUmMWQwdhrYpj0WRs9vhFtNIgEB4')
+conn=sqlitecloud.connect('sqlitecloud://cwcgjb0ahz.g1.sqlite.cloud:8860/chinook.sqlite?apikey=DaG8uyqMPa9GdxoR7ObMoajHIdfUOrc7B0mF0IrU6Y0')
 c=conn.cursor()
-placeholders = ','.join('?' for _ in dilsecimi)
-query = f"SELECT * FROM haberler WHERE dil IN ({placeholders}) ORDER BY trend_id DESC LIMIT 99"
-c.execute(query, dilsecimi)
 
-if len(ara) > 1:
-    if len(dilsecimi) > 0:
-        placeholders = ','.join(['?'] * len(dilsecimi))
-        query = f"SELECT * FROM haberler WHERE baslik LIKE ? AND dil IN ({placeholders}) ORDER BY trend_id DESC LIMIT 99"
-        params = [f"%{ara}%"] + dilsecimi
-        c.execute(query, params)
-    else:
-        c.execute("SELECT * FROM haberler WHERE baslik LIKE ? ORDER BY trend_id DESC LIMIT 99", (f"%{ara}%",))
+if len(ara)>1:
+    c.execute(f"SELECT * FROM haberler WHERE baslik LIKE '%{ara}%' ORDER BY trend_id DESC LIMIT 99 ")
+
 else:
-    if len(dilsecimi) > 0:
-        placeholders = ','.join(['?'] * len(dilsecimi))
-        query = f"SELECT * FROM haberler WHERE dil IN ({placeholders}) ORDER BY trend_id DESC LIMIT 99"
-        c.execute(query, dilsecimi)
-    else:
-        c.execute("SELECT * FROM haberler ORDER BY trend_id DESC LIMIT 99")
-else:
-    if len(dilsecimi) > 0:
-        placeholders = ','.join('?' for _ in dilsecimi)
-        query = f"SELECT * FROM haberler WHERE dil IN ({placeholders}) ORDER BY trend_id DESC LIMIT 99"
-        c.execute(query, dilsecimi)
-    else:
-        c.execute("SELECT * FROM haberler ORDER BY trend_id DESC LIMIT 99")
+    c.execute("SELECT * FROM haberler ORDER BY trend_id DESC LIMIT 99")
+    
+haberler=c.fetchall()
+
+if len(haberler)==0:
+    st.warning(f" {ara} sorgusu ile ilgili Herhangi Bir Haber bulunamadı")
+
+for i in range(0,len(haberler),3):
+    col1,col2,col3=st.columns(3)
+
+    with col1:
+        st.image(haberler[i][3])
+        st.write(haberler[i][1])
+        st.link_button("Habere Git",haberler[i][2])
+    with col2:
+        if i+1 < len(haberler):
+            st.image(haberler[i+1][3])
+            st.write(haberler[i+1][1])
+            st.link_button("Habere Git",haberler[i+1][2])
+        else:
+            pass
+
+    with col3:
+        if i+2 < len(haberler):
+            st.image(haberler[i+2][3])
+            st.write(haberler[i+2][1])
+            st.link_button("Habere Git",haberler[i+2][2])
+        else:
+            pass
